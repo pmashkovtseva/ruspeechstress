636338f7081a5f1fda39c27d4d4a6a9fe3016111
Polina Mashkovtseva <55890287+pmashkovtseva@users.noreply.github.com>
2023-06-08 09:44:25 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ruspeechstress',
-    version='0.7',
+    version='0.15',
     packages=find_packages(),
     url="https://github.com/pmashkovtseva/ruspeechstres",
     license='MIT',
